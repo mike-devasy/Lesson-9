eed07b56d75c6e9bfe8c4442720340c6ba8045dc
mike-devasy <108010984+mike-devasy@users.noreply.github.com>
2023-12-04 23:32:06 +0200

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="home">
+    <h2>Це магазин належить програмісту на фрілансі</h2>
+    <div>
+      Тому:
+      <ul>
+        <li> магазин працює коли хоче;</li>
+        <li> товари надсилає швидко;</li>
+        <li> на запитання відповідає коли висипається;</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {
+  },
+};
+</script>
